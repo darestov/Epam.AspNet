18c30cd681eb304d2609d41860d8bb9ca7d208d1
Dmitry Arestov <dmitry.arestov@itnetx.ch>
2020-03-15 22:45:56 +0300

    temp push

--- Epam.AspNet.Module1/Startup.cs
+++ Epam.AspNet.Module1/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Epam.AspNet.Module1
 {
@@ -22,11 +23,13 @@ namespace Epam.AspNet.Module1
             services.AddControllersWithViews();
             services.AddDbContext<NorthwindContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("local")));
+            services.AddLogging(b => b.AddProvider(new TrivialFileLoggingProvider("log.txt")));
         }
 
         // configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            logger.LogInformation("Application started from '{0}'", env.ContentRootPath);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ Epam.AspNet.Module1/TrivialFileLoggingProvider.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace Epam.AspNet.Module1
+{
+    internal class TrivialFileLoggingProvider : ILoggerProvider
+    {
+        private string fileName;
+
+        public TrivialFileLoggingProvider(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(categoryName, fileName);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    internal class FileLogger : ILogger
+    {
+        private string categoryName;
+        private string fileName;
+        object lockObj = new object();
+
+        public FileLogger(string categoryName, string fileName)
+        {
+            this.categoryName = categoryName;
+            this.fileName = fileName;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return new EmptyDisposable();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string msg = $"{logLevel} :: {categoryName} :: {formatter(state, exception)} :: username :: {DateTime.Now}";
+
+            lock (lockObj)
+            {
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine(msg);
+                }
+            }
+        }
+
+        private class EmptyDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+                
+            }
+        }
+    }
+}
