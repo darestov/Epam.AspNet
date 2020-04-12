238d1ba40889b076bcc958f808700a3d479b3f37
Dmitry Arestov <dmitry.arestov@itnetx.ch>
2020-04-12 21:05:33 +0300

    tag helpers

---
+++ Epam.AspNet.Module1/Helpers/ImageHelpers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epam.AspNet.Module1.Helpers
+{
+    public static class ImageHelpers
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper html, int imageId, string linkText)
+        {
+            return html.ActionLink(linkText, "Image", "Categories", new { id = imageId });
+        }
+    }
+}
---
+++ Epam.AspNet.Module1/Helpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epam.AspNet.Module1.Helpers
+{
+    [HtmlTargetElement(Attributes = "northwind-id")]
+    public class NorthwindImageTagHelper: TagHelper
+    {
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", "/Categories/Image/" + NorthwindId);
+        }
+    }
+}
